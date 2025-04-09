7c8079b79835c1286b87f5f660c70b8e78e7192a
ThabetheMT <mzwandileT21@gmail.com>
2025-04-09 21:00:54 +0100

    WORK AT HOME - 09 APRIL 2025

---
+++ src/main/java/com/uncle/kangarooquiz/QuizController.java
@@ -0,0 +1,21 @@
+package com.uncle.kangarooquiz;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@RestController
+public class QuizController {
+
+    @GetMapping("/quiz")
+    public String quiz(){
+        return "quiz";
+    }
+
+}
